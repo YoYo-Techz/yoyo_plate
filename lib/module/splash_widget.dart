68049f821e8f72fc39bc06c4aacb90cb7cf5efc4
Ko Htutz <info@kohtut.me>
2022-05-26 10:31:11 +0630

    New : Project Setup

---
+++ lib/module/splash_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashWiget extends StatefulWidget {
+  const SplashWiget({Key? key}) : super(key: key);
+
+  @override
+  State<SplashWiget> createState() => _SplashWigetState();
+}
+
+class _SplashWigetState extends State<SplashWiget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
